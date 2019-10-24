bc3bb7fb2e1b1733bead3e42a26c0d165f791946
Trevor <trevor.h.crow@gmail.com>
2019-10-24 02:01:11 -0400

    minor adjustments to limit param and inactive_wars_consec

--- CR_Clan_Activity.py
+++ CR_Clan_Activity.py
@@ -2,7 +2,7 @@ import json
 import requests
 from settings import API_KEY, API_URL, CLAN
 
-warlog = requests.get(API_URL+'clans/'+CLAN+'/warlog', headers={"Accept":"application/json", "authorization":API_KEY}, params={"limit":10}).json()
+warlog = requests.get(API_URL+'clans/'+CLAN+'/warlog', headers={"Accept":"application/json", "authorization":API_KEY}, params={"limit":3}).json()
 clanmembers = requests.get(API_URL+'clans/'+CLAN+'/members', headers={"Accept":"application/json", "authorization":API_KEY}).json()
 
 # consolidate these vars to a config file
@@ -10,7 +10,7 @@ inactive_wars_row = 3 # How many wars in a row can a player be inactive
 donation_threshold = 50 # minimum donations a player will need before the hit the threshold minimum
 # TODO: add a time variable for how long a player is allowed to be offline before their inactivity is recorded
 
-# find which players have been inactive consec based off inactive_wars_row and returns a bool
+# find which players have been inactive consec based off inactive_wars_row and return how many days in a row they have missed from the past 3 wars (excludes on-going)
 def inactive_wars_consec(player_tag, inactive_times):
     consec_inactive = 0 # wars so far inactive
     for war in warlog['items']:
@@ -22,11 +22,10 @@ def inactive_wars_consec(player_tag, inactive_times):
                 in_war = True
                 break
         if in_war == False:
-            consec_inactive += 1    
-    if consec_inactive >= inactive_times:
-        return True
-    else:
-        return False
+            consec_inactive += 1
+        else:
+            consec_inactive = 0
+    return consec_inactive
 
 with open('warlog.json', 'w') as json_file:
     json.dump(warlog, json_file)
@@ -34,9 +33,9 @@ with open('clanmembers.json', 'w') as json_file:
     json.dump(clanmembers, json_file)
 with open('player_donations.txt', 'w') as txt_file:
     for player in clanmembers['items']:
-        p_data = '%s: %d \n' % (player['tag'],player['donations'])
+        p_data = '%s - %a: %d\n' % (player['tag'], player['name'], player['donations'])
         txt_file.write(p_data)
 with open('player_war_inactivity.txt', 'w') as txt_file:
     for player in clanmembers['items']:
-        p_data = '%s: %d \n' % (player['tag'], inactive_wars_consec(player['tag'], inactive_wars_row))
+        p_data = '%s - %a: %d\n' % (player['tag'], player['name'], inactive_wars_consec(player['tag'], inactive_wars_row))
         txt_file.write(p_data)
